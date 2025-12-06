3559139867c6eecfea3d6c73df8686abce4af939
reza <rm2997@gmail.com>
2025-12-06 13:36:13 +0330

    server cors fixed - 14040915

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,42 +1,26 @@
 import axios from "axios";
 import { loadTokens } from "./tokenUtils";
-
-//const BASE_URL = "http://localhost:3001";
 const BASE_URL =
   process.env.NODE_ENV == "production"
     ? "https://api.hesab-yaar.ir"
     : "http://localhost:3001";
 const axiosClient = axios.create({
   baseURL: `${BASE_URL}`,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
+
 });
 
 axiosClient.interceptors.request.use((config) => {
   const { accessToken } = loadTokens();
-
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
   return config;
 });
 
-// axiosClient.interceptors.response.use(
-//   (response) => response,
-//   async (err) => {
-//     if (err.response.status === 401) {
-//       clearTokens();
-//       if (
-//         window.location.href.endsWith("/login") === false ||
-//         window.location.href.endsWith("/second-login") === false
-//       )
-//         window.location.href = "/login";
-//       return Promise.reject(err);
-//     }
-//     return Promise.reject(err);
-//   }
-// );
 
 export const apiRequest = async ({
   method,
@@ -105,40 +89,5 @@ export const apiRequest = async ({
   }
 };
 
-// axiosClient.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
-
-//     // اگر 401 خطا گرفتیم و هنوز توکن رفرش داریم
-//     if (
-//       error.response.status === 401 &&
-//       originalRequest &&
-//       !originalRequest._retry
-//     ) {
-//       originalRequest._retry = true;
-//       console.log("401 response recieved");
-//       const { refreshToken } = loadTokens(); // توکن رفرش از localStorage
-//       try {
-//         const { access_token } = await refreshTokens(refreshToken);
-//         saveTokens(access_token);
-
-//         // تنظیم هدر با accessToken جدید
-//         axios.defaults.headers["Authorization"] = `Bearer ${access_token}`;
-
-//         // مجدداً درخواست اصلی را ارسال می‌کنیم
-//         return axiosClient(originalRequest);
-//       } catch (error) {
-//         // اگه توکن رفرش هم معتبر نباشه یا درخواست رفرش توکن شکست خورد
-//         // باید لاگ‌اوت کنیم
-//         console.error("Error refreshing token:", error);
-//         window.location.href = "/login";
-//         return Promise.reject(error);
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
 
 export default axiosClient;
